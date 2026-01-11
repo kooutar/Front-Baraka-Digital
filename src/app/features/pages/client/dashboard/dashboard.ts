7d6a6361d710819caa3aa390172fd82ad46c1524
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2026-01-11 21:32:07 +0100

    feat : implementer logique de recupere user connecter

--- src/app/features/pages/client/dashboard/dashboard.ts
+++ src/app/features/pages/client/dashboard/dashboard.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { SidebarClient } from '../../../components/client/sidebar-client/sidebar-client';
+import { User } from '../../../auth/models/response/user';
+import { UserService } from '../../../../core/services/user/user-service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +10,10 @@ import { SidebarClient } from '../../../components/client/sidebar-client/sidebar
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
-  
 
+  user: User | null;
+
+  constructor(@Inject(UserService) private userService: any) {
+    this.user = this.userService.getUserConected();
+  }
 }
