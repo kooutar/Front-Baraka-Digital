ca8201e972624a66eb74fa51fe68368ddc87d1b6
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2026-01-11 11:18:07 +0100

    delete : supprime app/clien dachboard  et le repmlacer par feautares/pages/cleint/dachboard ei modifier routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 
-import { ClientDashboard } from './client-dashboard/client-dashboard';
 import { Login } from './features/auth/pages/login/login';
+import  {Dashboard} from './features/pages/client/dashboard/dashboard';
 
 export const routes: Routes = [
 
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: Login },
-  
-    {path:'clientDashboard',component: ClientDashboard}
+    {path:'client/dashboard',component:Dashboard}
 
     
 ];
